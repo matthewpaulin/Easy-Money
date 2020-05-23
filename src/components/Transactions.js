9822909a0f20e96e16cad02f1d49dcc8a9f16549
Matthew Paulin <matthewjpaulin@gmail.com>
2020-05-22 23:41:28 -0400

    added transaction sorting and deployed to firebase

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,43 +1,64 @@
-import React, {useEffect, useState, useContext, useRef} from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import {Transaction} from './Transaction';
 import firebase from '../firebase';
 import {GlobalContext} from '../context/GlobalState';
 
-function useIsMountedRef(){
-    const isMountedRef = useRef(null);
-    useEffect(() => {
-      isMountedRef.current = true;
-      return () => isMountedRef.current = false;
-    });
-    return isMountedRef;
-}
-
 function AcctData(id){
     const [account, setAccount] = useState([]);
-    const isMountedRef = useIsMountedRef();
 
     useEffect(() =>{
+        const unsub = 
         firebase.firestore().collection('accounts').doc(id)
             .onSnapshot((snapshot)=>{
                 const newAccount = snapshot.get("transactions")
-                if(isMountedRef.current){
                     setAccount(newAccount);
-                }
             })
-    }, [id, isMountedRef]);
+            return () => unsub();
+    }, [id]);
     
     return account;
 }
 
-
-
 export const Transactions = () => {
     const {currentAcc} = useContext(GlobalContext);
-    const transactions = AcctData(currentAcc)
-
+    const [sortBy, setSortBy] = useState('DATE_DESC');
+    const unsortedTransactions = AcctData(currentAcc)
+    const transactions = unsortedTransactions.sort(function(a,b){
+        switch(sortBy){
+            case "DATE_DESC":
+                return b.time - a.time;
+            case "DATE_ASC":
+                return a.time - b.time;
+            case "NAME_ASC":
+                return a.tName>b.tName ? 1 : -1;
+            case "NAME_DESC":
+                return a.tName>b.tName ? -1 : 1;
+            case "AMOUNT_DESC":
+                return b.tVal - a.tVal;
+            case "AMOUNT_ASC":
+                return a.tVal - b.tVal;
+            default:
+                return b.time - a.time;
+        }
+    })
     return (
         <div id="transaction-container">
-            <h3>History</h3>
+            <div className="sort">
+                <h3>History</h3>
+                <div>
+                    <label>Sort By:</label>{" "}
+                    <select value={sortBy} onChange={e => setSortBy(e.currentTarget.value)}> 
+                        <option value='DATE_DESC'> Date (newest first)</option>
+                        <option value='DATE_ASC'> Date (oldest first)</option>
+                        <option disabled>--------</option>
+                        <option value='NAME_ASC'> Name (A - Z)</option>
+                        <option value='NAME_DESC'> Name (Z - A)</option>
+                        <option disabled>--------</option>
+                        <option value='AMOUNT_DESC'> Amount ($$$-$)</option>
+                        <option value='AMOUNT_ASC'> Amount ($-$$$)</option>
+                    </select>
+                </div>
+            </div>
             <ul className="list">
                 {transactions.map(transaction => (
                     <Transaction key={transaction.tID} transaction={transaction}/>
